92819617d3af75b759aa7e86418710e73ae6bbe9
DSRoden <Daniel.S.Roden@gmail.com>
2015-03-29 21:36:53 -0400

    added headers and footers, and header disappears on scrolling down while reappearing when scrolling up

--- www/js/controllers/home-controller.js
+++ www/js/controllers/home-controller.js
@@ -2,10 +2,14 @@
   'use strict';
 
   angular.module('spotlight-famous')
-  .controller('HomeCtrl', ['$scope', '$timeout', '$location', '$famous', function($scope, $timeout, $location, $famous){
-     var EventHandler = $famous['famous/core/EventHandler'];
+  .controller('HomeCtrl', ['$scope', '$timeout', '$location', '$famous', '$timeline', function($scope, $timeout, $location, $famous, $timeline){
+    //shows and hide scope variables
+    $scope.spotlightHeaderOn =  true;
+    $scope.spotlightFooterOn = true;
 
-    $scope.views = [{color: 'red'}, {color: 'blue'}, {color: 'green'}, {color: 'yellow'}, {color: 'orange'}];
+    var EventHandler = $famous['famous/core/EventHandler'];
+
+    $scope.views = [{color: 'red'}, {color: 'blue'}, {color: 'green'}, {color: 'yellow'}, {color: 'orange'}, {color: 'red'}, {color: 'blue'}, {color: 'green'}, {color: 'yellow'}, {color: 'orange'}];
 
     $scope.myEventHandler = new EventHandler();
     $scope.eventHandler = new EventHandler();
@@ -31,6 +35,52 @@
       }
     };
 
+    //transitions and animations
+    // var Transitionable = $famous['famous/transitions/Transitionable'],
+    // Easing = $famous['famous/transitions/Easing'];
 
+    // $scope.t = new Transitionable(0);
+
+    // $scope.yRotation = $timeline([
+    //   [0, 0, Easing.inOutQuad],
+    //   [0.8, 1.1 * (Math.PI/2), Easing.inOutQuart],
+    //   [1, Math.PI/2]
+    // ]);
+
+    // $scope.translation = $timeline([
+    //   [0, [100, 100, 0], Easing.inOutQuad],
+    //   [1, [400, 200, 0]]
+    // ]);
+    //fading somethign out
+    var scrollView;
+    $scope.scrollViewHandler = new EventHandler();
+    $timeout(function(){
+      scrollView = $famous.find('#main-scrollview')[0].renderNode;
+      $scope.scrollViewHandler = scrollView.sync;
+       $scope.scrollViewHandler.on('start', function(event){
+          console.log('start', event);
+          console.log('start event position', scrollView.getPosition());
+          console.log('start event velocity', scrollView.getVelocity());
+          // if(scrollView.getPosition() >= 41){
+          //   $scope.spotlightHeaderOn=  false;
+          //   $scope.$digest();
+          // }
+        });
+       $scope.scrollViewHandler.on('end', function(event){
+          console.log('end event', event);
+          console.log('end event position', scrollView.getPosition());
+          console.log('end event velocity', scrollView.getVelocity());
+          // if(scrollView.getVelocity() >= 0){
+          //   $scope.spotlightHeaderOn = true;
+          if(scrollView.getVelocity() > 0){
+            console.log('turn header off');
+            $scope.spotlightHeaderOn= false;
+            $scope.$digest();
+          } else {
+            $scope.spotlightHeaderOn= true;
+            $scope.$digest();
+          }
+        });
+    }, 1000);
   }]);
 })();
