58bd3a8c6b5c19460e95be5e7b8669849ae6cb4f
DSRoden <Daniel.S.Roden@gmail.com>
2015-03-31 00:48:32 -0400

    archive page with scroll and disappearing navbard and spotlight icon

--- www/js/controllers/home-controller.js
+++ www/js/controllers/home-controller.js
@@ -10,6 +10,10 @@
     $scope.boxTransitionableHeader = new Transitionable([0, 0, 0]);
     $scope.boxTransitionableFooter = new Transitionable([0, 0, 0]);
     $scope.opacityTrans = new Transitionable(1);
+    $scope.boxTransitionableHeader2 = new Transitionable([0, 0, 0]);
+    $scope.boxTransitionableFooter2 = new Transitionable([0, 0, 0]);
+    $scope.opacityTrans2 = new Transitionable(1);
+
     $scope.myGridLayoutOptions = {
        dimensions: [1,1] // specifies number of columns and rows
     };
@@ -22,14 +26,17 @@
                       bgColor: 'rgb(240, 238, 233)'
                     }];
 
+
     //shows and hide scope variables
     $scope.spotlightHeaderOn =  true;
 
     var EventHandler = $famous['famous/core/EventHandler'];
 
     $scope.views = [{color: 'gray'}, {color: 'white'}, {color: 'gray'}, {color: 'white'}, {color: 'gray'}, {color: 'white'}, {color: 'gray'}, {color: 'white'}, {color: 'gray'}, {color: 'white'}];
+    $scope.archiveViews = [{color: 'gray'}, {color: 'white'}, {color: 'gray'}, {color: 'white'}, {color: 'gray'}, {color: 'white'}, {color: 'gray'}, {color: 'white'}, {color: 'gray'}, {color: 'white'}];
 
     $scope.myEventHandler = new EventHandler();
+    $scope.archiveScrollEventHandler = new EventHandler();
     $scope.eventHandler = new EventHandler();
     $scope.list = [{content: 'famous'}, {content: 'angular'}, {content: 'rocks!'}];
     $scope.getPageWidth = function(){
@@ -81,6 +88,38 @@
             $scope.boxTransitionableFooter.set([0, 0, 0], {duration: 300, curve: Easing.easeIn});
             $scope.opacityTrans.set([1], {duration: 300});
             $scope.$digest();
+          }
+        });
+    }, 1000);
+
+    var archiveScrollView;
+    $scope.scrollViewHandlerArchive = new EventHandler();
+    $timeout(function(){
+      archiveScrollView = $famous.find('#archive-scrollview')[0].renderNode;
+      $scope.scrollViewHandlerArchive = archiveScrollView.sync;
+       $scope.scrollViewHandlerArchive.on('start', function(event){
+          // console.log('start', event);
+          // console.log('start event position', scrollView.getPosition());
+          // console.log('start event velocity', scrollView.getVelocity());
+        });
+       $scope.scrollViewHandlerArchive.on('end', function(event){
+          // console.log('end event', event);
+          // console.log('end event position', scrollView.getPosition());
+          // console.log('end event velocity', scrollView.getVelocity());
+          var archiveWindowHeight = window.innerHeight;
+          if(archiveScrollView.getVelocity() > 0){
+            console.log('turn header off');
+            $scope.boxTransitionableHeader2.set([0, -40, 0], {duration: 300, curve: Easing.easeOut});
+            $scope.boxTransitionableFooter2.set([0, archiveWindowHeight, 0], {duration: 300, curve: Easing.easeOut});
+            $scope.opacityTrans2.set([0], {duration: 300});
+            $scope.$digest();
+          } else if(archiveScrollView.getVelocity() <= 0){
+            $scope.boxTransitionableHeader2.set([0, 0, 0], {duration: 300, curve: Easing.easeIn});
+            $scope.boxTransitionableFooter2.set([0, 0, 0], {duration: 300, curve: Easing.easeIn});
+            $scope.opacityTrans2.set([1], {duration: 300});
+            $scope.$digest();
+          } else {
+            return;
           }
         });
     }, 1000);
